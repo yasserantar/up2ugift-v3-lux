3431bbdcfd94c5e6fa4fa068cdae60360d4ecacd
yaser antar [art director] <Y.antar@AWT.DOM>
2026-08-02 10:19:04 +0300

    fix: switch to cloud Postgres DB

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,8 @@
-import 'dotenv/config'
-import { defineConfig, env } from 'prisma/config'
+import { defineConfig } from 'prisma/config'
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
   datasource: {
-    url: env('DATABASE_URL'),
+    url: "postgres://cad3bfa022248fc92c9681b3c5bb3cc3079e70c37200c7d9ee888d433a4740f7:sk_-5FnkPtMbYK9Tu-9PWA0J@db.prisma.io:5432/postgres?sslmode=require",
   },
 })
